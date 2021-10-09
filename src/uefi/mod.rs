ea0c4faf1e4f7dc28dcab65381359a3396aec1d3
k-nasa <htilcs1115@gmail.com>
2021-10-09 16:10:52 +0900

    remove comment

--- src/uefi/mod.rs
+++ src/uefi/mod.rs
@@ -2,12 +2,9 @@ use core::ffi::c_void;
 
 pub struct Handle(*mut c_void);
 
-// NOTE
-// UINT64 Signature;
-// UINT32 Revision;
-// UINT32 HeaderSize;
-// UINT32 CRC32;
-// UINT32 Reserved;
+#[repr(C)]
+struct SystemTable {}
+
 #[repr(C)]
 struct Header {
     signature: u64,
